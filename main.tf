aa2d4aaec61ae735d389620973530b810aade9dd
pavanbairu <pavan.bairu1@gmail.com>
2023-11-05 14:41:08 +0530

    comment web

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ module "vpc" { # calling the vpc module from git
   env        = var.env                  # env
   tags       = local.tags               # tags from local
 }
-
-module "web" {
-  source = "git::https://github.com/bairupavan/tf-module-app.git"
-  for_each = var.app
-  instance_type = each.value["instance_type"]
-  subnet_id = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
-}
+#
+#module "web" {
+#  source = "git::https://github.com/bairupavan/tf-module-app.git"
+#  for_each = var.app
+#  instance_type = each.value["instance_type"]
+#  subnet_id = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
+#}
